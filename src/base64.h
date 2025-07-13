11b66b6a63a24835d973a92ae9258d891a161f93
OlehKniaziev <atmatm9182@gmail.com>
2025-07-13 11:32:09 +0200

    Forgor C++ guards 💀

--- src/base64.h
+++ src/base64.h
@@ -3,7 +3,15 @@
 
 #include "common.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 b32 WebBase64Decode(web_string_view InputBuffer, u8 *OutputBuffer, uz *OutputBufferCount);
 void WebBase64Encode(web_string_view InputBuffer, u8 *OutputBuffer, uz *OutputBufferCount);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BASE64_H_

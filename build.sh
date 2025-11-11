0a0e04ce46763fc9e5cb01037ceaa2c3128c25b5
OlehKniaziev <atmatm9182@gmail.com>
2025-11-11 20:41:20 +0100

    Changed the `-ggdb` flag to `-g`

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -xe
 
-cc -ggdb -Wall -Wextra -Werror -pedantic -Og -c -fpic src/http.c src/json.c src/common.c src/base64.c src/threadpool.c
+cc -g -Wall -Wextra -Werror -pedantic -Og -c -fpic src/http.c src/json.c src/common.c src/base64.c src/threadpool.c
 ar rcs libweb.a http.o json.o common.o base64.o threadpool.o
